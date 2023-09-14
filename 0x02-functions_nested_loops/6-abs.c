38dd2f4ab0f1886a19d02cd40dccb1c94a220495
daylight <assawangel@gmail.com>
2023-09-13 17:40:57 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - main function
+ * Return: -m or m
+ * @m: function parameter
+ */
+
+int _abs(int m)
+{
+	if (m < 0)
+	return (-m);
+	else
+	{
+		return (m);
+	}
+}
