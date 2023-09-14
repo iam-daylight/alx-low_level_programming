bba71c1e85376b1fe3fd7a175e6bc48d01b71e1b
daylight <assawangel@gmail.com>
2023-09-13 18:06:33 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - main function
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int m, n;
+
+	for (m = 0; m < 24; m++)
+	{
+		for (n = 0; n < 60; n++)
+		{
+			_putchar (m / 10 + '0');
+			_putchar (m % 10 + '0');
+			_putchar (':');
+			_putchar (n / 10 + '0');
+			_putchar (n % 10 + '0');
+			_putchar ('\n');
+		}
+	}
+}
